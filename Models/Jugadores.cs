30133ac519d0d678ef9485895156926d32480ed8
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-02 12:06:37 -0400

    Se creo el modelo jugadores con sus respectivos atributos

---
+++ Models/Jugadores.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestionJugadores.Models
+{
+    public class Jugadores
+    {
+        [Key]
+
+        public int JugadorId {  get; set; }
+
+        [Required(ErrorMessage = "Campo requerido")]
+
+        public String Nombres { get; set; } = null!;
+
+        [Required(ErrorMessage = "Campo requerido")]
+
+        public int Partidas { get; set; }
+    }
+}
