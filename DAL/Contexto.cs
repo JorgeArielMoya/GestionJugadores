3c559fb9d63e196124d3c12e62d635e74d63222e
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-02 12:25:54 -0400

    Creamos nuestro contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using GestionJugadores.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionJugadores.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto (DbContextOptions<Contexto> options) : base(options) { }
+
+        public DbSet<Jugadores> Jugadores { get; set; }
+    }
+}
