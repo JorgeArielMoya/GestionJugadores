d1a380d93f59db69fca25717d65a1aad24fdbd60
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-05 13:51:50 -0400

    Se ajustaron algunas lineas

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -1,19 +1,17 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
 
 namespace GestionJugadores.Models
 {
     public class Jugadores
     {
         [Key]
-
         public int JugadorId {  get; set; }
 
         [Required(ErrorMessage = "Campo requerido")]
-
         public String Nombres { get; set; } = null!;
 
         [Range(0, int.MaxValue, ErrorMessage = "Numero de partidas no valido")]
-
-        public int Partidas { get; set; }
+        public int? Partidas { get; set; }
     }
 }
