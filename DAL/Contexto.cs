2346b36f2f5be17c3a7d4582a7ca331777a2b442
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-10 20:51:01 -0400

    Aplicamos file scoped name space en los archivos .cs

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -1,12 +1,11 @@
 ﻿using GestionJugadores.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace GestionJugadores.DAL
-{
-    public class Contexto : DbContext
-    {
-        public Contexto (DbContextOptions<Contexto> options) : base(options) { }
+namespace GestionJugadores.DAL;
 
-        public DbSet<Jugadores> Jugadores { get; set; }
-    }
+public class Contexto : DbContext
+{
+    public Contexto (DbContextOptions<Contexto> options) : base(options) { }
+
+    public DbSet<Jugadores> Jugadores { get; set; }
 }
--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -1,17 +1,15 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.AspNetCore.Mvc;
 
-namespace GestionJugadores.Models
+namespace GestionJugadores.Models;
+
+public class Jugadores
 {
-    public class Jugadores
-    {
-        [Key]
-        public int JugadorId {  get; set; }
+    [Key]
+    public int JugadorId {  get; set; }
 
-        [Required(ErrorMessage = "Campo requerido")]
-        public String Nombres { get; set; } = null!;
+    [Required(ErrorMessage = "Campo requerido")]
+    public String Nombres { get; set; } = null!;
 
-        [Range(0, int.MaxValue, ErrorMessage = "Numero de partidas no valido")]
-        public int Partidas { get; set; }
-    }
+    [Range(0, int.MaxValue, ErrorMessage = "Numero de partidas no valido")]
+    public int Partidas { get; set; }
 }
