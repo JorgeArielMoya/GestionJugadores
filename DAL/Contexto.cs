bfbd869202570cd1d072989498ee4020c24dd267
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-11 20:20:06 -0400

    Se agrego el DbSet partidas

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,6 +6,9 @@ namespace GestionJugadores.DAL;
 public class Contexto : DbContext
 {
     public DbSet<Jugadores> Jugadores { get; set; }
+
+    public DbSet<Partidas> Partidas { get; set; }
+
     public Contexto (DbContextOptions<Contexto> options) : base(options) { }
 
 }
