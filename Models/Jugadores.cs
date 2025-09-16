67c7bc51e8333b66fe57c0f4dc767bd512efa056
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-15 20:17:05 -0400

    Se agregaron los campos de derrotas y empates

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -11,5 +11,9 @@ public class Jugadores
     public String Nombres { get; set; } = null!;
 
     [Range(0, int.MaxValue, ErrorMessage = "Numero de partidas no valido")]
-    public int Victorias { get; set; }
+    public int Victorias { get; set; } = 0;
+
+    public int Derrotas { get; set; } = 0;
+
+    public int Empates { get; set; } = 0;
 }
