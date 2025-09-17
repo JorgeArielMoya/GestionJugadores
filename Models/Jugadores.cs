b4c7474229302b2572f57e85d9fddd62a39b433c
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-16 21:25:04 -0400

    Se agrego el ICollection de movimiento

--- Models/Jugadores.cs
+++ Models/Jugadores.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace GestionJugadores.Models;
 
@@ -16,4 +17,7 @@ public class Jugadores
     public int Derrotas { get; set; } = 0;
 
     public int Empates { get; set; } = 0;
+
+    [InverseProperty(nameof(Movimientos.Jugador))]
+    public virtual ICollection<Movimientos> Movimiento { get; set; } = new List<Movimientos>();    
 }
