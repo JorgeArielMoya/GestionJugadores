2e1753faf24e4e86c5b746a1de2d6a45b85249f4
jorge <jorge@LAPTOP-D8GDLQRL>
2025-09-17 18:36:48 -0400

    File scopedNamespace aplicado

--- Models/Movimientos.cs
+++ Models/Movimientos.cs
@@ -1,31 +1,30 @@
 ﻿using System.ComponentModel.DataAnnotations.Schema;
 using System.ComponentModel.DataAnnotations;
 
-namespace GestionJugadores.Models
+namespace GestionJugadores.Models;
+
+public class Movimientos
 {
-    public class Movimientos
-    {
-        [Key]
-        public int MovimientoId { get; set; }
+    [Key]
+    public int MovimientoId { get; set; }
 
-        public int PartidaId { get; set; }
+    public int PartidaId { get; set; }
 
-        public int JugadorId { get; set; }
+    public int JugadorId { get; set; }
 
-        [Range(0, 2)]
-        public int PosicionFila { get; set; }
+    [Range(0, 2)]
+    public int PosicionFila { get; set; }
 
-        [Range(0, 2)]
-        public int PosicionColumna { get; set; }
+    [Range(0, 2)]
+    public int PosicionColumna { get; set; }
 
-        public DateTime FechaMovimiento { get; set; } = DateTime.Now;
+    public DateTime FechaMovimiento { get; set; } = DateTime.Now;
 
-        //Propiedades de Navegacion
-        [ForeignKey(nameof(PartidaId))]
-        public virtual Partidas Partida { get; set; }
+    //Propiedades de Navegacion
+    [ForeignKey(nameof(PartidaId))]
+    public virtual Partidas Partida { get; set; }
 
-        [ForeignKey(nameof(JugadorId))]
-        public virtual Jugadores Jugador { get; set; }
-    }
+    [ForeignKey(nameof(JugadorId))]
+    public virtual Jugadores Jugador { get; set; }
 }
 
