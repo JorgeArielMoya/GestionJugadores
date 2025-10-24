c394415964f64f3a58fb33595e1e924da544ac67
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-24 18:53:49 -0400

    inyectamos el api y el service de partidas

--- GestionJugadores.BlazorWasm/Program.cs
+++ GestionJugadores.BlazorWasm/Program.cs
@@ -1,4 +1,5 @@
 using GestionJugadores.BlazorWasm;
+using GestionJugadores.BlazorWasm.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,8 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
+
+builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
 
 await builder.Build().RunAsync();
