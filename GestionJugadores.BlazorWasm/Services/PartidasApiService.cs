cd90ec67064133560da476fe4af06d9a0e34f316
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-25 12:06:34 -0400

    Volvio a su estado original

--- GestionJugadores.BlazorWasm/Services/PartidasApiService.cs
+++ GestionJugadores.BlazorWasm/Services/PartidasApiService.cs
@@ -1,6 +1,8 @@
 ﻿using GestionJugadores.Shared.Dtos;
 using GestionJugadores.Shared;
 using System.Net.Http.Json;
+using System.Text.Json;
+using System.Text;
 
 namespace GestionJugadores.BlazorWasm.Services;
 
