7da4bd8b3fbd96ab625b6f336871ff4503ff9327
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-25 12:43:00 -0400

    Se quito una inyeccion del program

--- GestionJugadores.BlazorWasm/Program.cs
+++ GestionJugadores.BlazorWasm/Program.cs
@@ -10,7 +10,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/") });
 
 builder.Services.AddScoped<IPartidasApiService, PartidasApiService>();
-builder.Services.AddScoped<IJugadorApiService, JugadoresApiService>();
 builder.Services.AddScoped<IMovimientoApiService, MovimientosApiService>();
 
 await builder.Build().RunAsync();
