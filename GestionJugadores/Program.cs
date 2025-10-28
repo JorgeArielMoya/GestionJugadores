0f3f720bfe6515cf0dda21339f9f568c5d1b2d29
jorge <jorge@LAPTOP-D8GDLQRL>
2025-10-27 22:05:14 -0400

    Guardar en Github

--- GestionJugadores/Program.cs
+++ GestionJugadores/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+//Inyeccion del Api
 builder.Services.AddHttpClient("ApiGestionHuacales", client =>
 {
     client.BaseAddress = new Uri("https://gestionhuacalesapi.azurewebsites.net/");
